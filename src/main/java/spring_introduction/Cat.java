4614ba9495c2d8a4cc3546f3b8778b0c9702e700
alami <alami@km.ru>
2020-11-26 17:50:40 +0500

    add console color class

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -1,9 +1,9 @@
 package spring_introduction;
 public class Cat implements Pet{
     public Cat (){
-        System.out.println("Cat bean is created");
+        System.out.println(color.YELLOW+"Cat bean is created"+color.RESET);
     }    @Override
     public void say() {
-        System.out.println("Meow-Meow");
+        System.out.println(color.YELLOW+"Meow-Meow"+color.RESET);
     }
 }
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,15 +3,15 @@ package spring_introduction;
 public class Person {
     private Pet pet;
     public Person (/*Pet pet*/) {
-        System.out.println("Person bean is created");
+        System.out.println(color.GREEN+"Person bean is created"+color.RESET);
         this.pet = pet;
     }
     public void setPet(Pet pet) {
-        System.out.println("Class Person : set pet");
+        System.out.println(color.GREEN+"Class Person : set pet"+color.RESET);
         this.pet = pet;
     }    
     public void callYourPet () {
-        System.out.println("Hello, my lovely Pet!");
+        System.out.println(color.GREEN+"Hello, my lovely Pet!"+color.RESET);
         pet.say();
     }    
 }
