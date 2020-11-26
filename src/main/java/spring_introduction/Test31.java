4efd9b3e31795f2bc3b2b9b1c1f6aaeb310973b3
alami <alami@km.ru>
2020-11-26 17:30:37 +0500

    ch01-07fin

---
+++ src/main/java/spring_introduction/Test31.java
@@ -0,0 +1,24 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test31 {
+
+    public static void main(String[] args) {
+
+//        Pet pet = new Dog();
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+////        Pet pet = context.getBean("myPet", Pet.class);
+//        pet.say();
+////        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+
+        context.close();
+     
+//        Person person1 = new Person(new Cat());
+//        person1.callYourPet();
+    }
+
+}
