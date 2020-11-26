ae98da180857607f0e9f1a21ee9423065533b704
alami <alami@km.ru>
2020-11-26 19:34:30 +0500

    ch01-10 scope = sigletone

---
+++ src/main/java/spring_introduction/Test4.java
@@ -0,0 +1,30 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+
+//        Pet pet = new Dog();
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+////        Pet pet = context.getBean("myPet", Pet.class);
+        Dog myDog  = context.getBean("myPet", Dog.class);
+        myDog.setName("Belka");
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.setName("Strelka");
+//        pet.say();
+////        Person person = new Person(pet);
+        System.out.println("Is the same object ?" + (myDog==yourDog));
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
+
+
+         context.close();
+     
+//        Person person1 = new Person(new Cat());
+//        person1.callYourPet();
+    }
+
+}
