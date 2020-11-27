c6b2cdb5f88826a0c5a8c2be50ef3f96e29a90a6
Aminov <alami@tshtt.uz>
2020-11-27 07:37:11 +0500

    ch01-19 annotation @Scome init destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,8 +1,13 @@
 package spring_introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class Dog implements Pet{
 //    private String name;
     public Dog() {
@@ -22,9 +27,11 @@ public class Dog implements Pet{
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("Class Dog: init method");
     }
+    @PreDestroy
     public void destroy() {
         System.out.println("Class Dog: destroy method");
     }
