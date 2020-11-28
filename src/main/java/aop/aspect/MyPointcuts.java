34383d194246daae3c911a8295cb7c31998f1a33
Aminov <alami@tshtt.uz>
2020-11-28 14:54:53 +0500

    ch02-09. Advice @AfterReturning change result!

--- src/main/java/aop/aspect/MyPointcuts.java
+++ src/main/java/aop/aspect/MyPointcuts.java
@@ -3,7 +3,7 @@ package aop.aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointcuts {
-    @Pointcut("execution(public void add* (..))")
+    @Pointcut("execution(public void abc* (..))")
     public void allAddMethods(){}
 
 }
---
+++ src/main/java/aop/aspect/UniversityLoggingAspect.java
@@ -0,0 +1,39 @@
+package aop.aspect;
+
+import aop.Book;
+import aop.Student;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import spring_introduction.color;
+
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsAdvice () {
+        System.out.println(color.YELLOW+"beforeGetStudentsAdvice: " +
+                "логируем получение списка студентов перед методом getStudets");
+    }
+    @AfterReturning(pointcut="execution(* getStudents())"
+                , returning = "students")
+    public void afterReturningGetStudentsAdvice (List<Student> students) {
+        Student firstStudent = students.get(0);
+        String nameSurname = firstStudent.getName();
+        nameSurname = "Mr."+ nameSurname;
+        firstStudent.setName(nameSurname);
+
+        double avrGrade = firstStudent.getAvgGrade();
+        avrGrade = avrGrade +1;
+        firstStudent.setAvgGrade(avrGrade);
+
+        System.out.println(color.YELLOW+"afterReturningGetStudentsAdvice: " +
+                "логируем получение списка студентов после метода  getStudets");
+    }
+}
