8ea52b40a25bd47dcc434d36134f84f52a05556c
Aminov <alami@tshtt.uz>
2020-11-28 21:47:32 +0500

    ch03-07 session add

--- src/main/java/hibernate_test/Test2.java
+++ src/main/java/hibernate_test/Test2.java
@@ -6,7 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import spring_introduction.color;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -15,12 +15,19 @@ public class Test1 {
         try {
             Session session = factory.getCurrentSession();
 //            Employee emp = new Employee("Zaur", "Tregulov", "IT", 500);
-            Employee emp = new Employee("Alex", "Ivanov", "IT", 600);
+            Employee emp = new Employee("Elene", "Petrova", "Sales", 800);
             session.beginTransaction();
             session.save(emp);
+//            session.getTransaction().commit();
+
+            int myId = emp.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee employee = session.get(Employee.class, myId );
             session.getTransaction().commit();
+
             System.out.println("Done!");
-            System.out.println(color.BLUE+emp+color.RESET);
+            System.out.println(color.BLUE+employee+color.RESET);
         } finally {
             factory.close();
         }
