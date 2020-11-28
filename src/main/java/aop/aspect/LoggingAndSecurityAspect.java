c3fe53cc89f4240ea8f9d2efd902a8f86eb66c0f
Aminov <alami@tshtt.uz>
2020-11-28 10:12:36 +0500

    ch02-05 pointcut combination

--- src/main/java/aop/aspect/LoggingAndSecurityAspect.java
+++ src/main/java/aop/aspect/LoggingAndSecurityAspect.java
@@ -10,32 +10,52 @@ import spring_introduction.color;
 @Component
 @Aspect
 public class LoggingAndSecurityAspect {
-    @Pointcut("execution(* aop.UniLibrary.get*())")
-    private void allGetMethodsFromLibrary(){}
 
-    @Pointcut("execution(* aop.UniLibrary.return*())")
-    private void allReturnMethodsFromLibrary(){}
+    @Pointcut("execution(* aop.UniLibrary.*(..))")
+    private void allMethodsFromLibrary(){}
+    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+    private void returnMagazineToLibrary(){}
+    @Pointcut("allMethodsFromLibrary() && !returnMagazineToLibrary()")
+    private void allMethodsExceptReturnMagazine(){}
 
-    @Pointcut("allGetMethodsFromLibrary() || allReturnMethodsFromLibrary()")
-    private void allGetAndReturnMethodsFromLibrary(){}
-
-    @Before("allGetMethodsFromLibrary()")
+    @Before("allMethodsFromLibrary()")
     public void beforeGetLoggingAdvice(){
         System.out.println(color.BLUE+"beforeGetLoggingAdvice: " +
                 "writing Log #1"+color.RESET);
     }
 
-    @Before("allGetAndReturnMethodsFromLibrary()")
+    @Before("allMethodsExceptReturnMagazine()")
     public void beforeGetAndReturnLoggingAdvice(){
         System.out.println(color.BLUE+"beforeGetAndReturnLoggingAdvice: " +
-                "writing Log #3"+color.RESET);
+                "writing Log #10"+color.RESET);
     }
 
-    @Before("allReturnMethodsFromLibrary()")
-    public void beforeReturnLoggingAdvice(){
-        System.out.println(color.BLUE+"beforeReturnLoggingAdvice: " +
-                "writing Log #2"+color.RESET);
-    }
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allGetMethodsFromLibrary(){}
+//
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromLibrary(){}
+//
+//    @Pointcut("allGetMethodsFromLibrary() || allReturnMethodsFromLibrary()")
+//    private void allGetAndReturnMethodsFromLibrary(){}
+//
+//    @Before("allGetMethodsFromLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println(color.BLUE+"beforeGetLoggingAdvice: " +
+//                "writing Log #1"+color.RESET);
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println(color.BLUE+"beforeGetAndReturnLoggingAdvice: " +
+//                "writing Log #3"+color.RESET);
+//    }
+//
+//    @Before("allReturnMethodsFromLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println(color.BLUE+"beforeReturnLoggingAdvice: " +
+//                "writing Log #2"+color.RESET);
+//    }
 
 //    @Pointcut("execution(public void get* ())")
 //    private void allGtMethods(){}
